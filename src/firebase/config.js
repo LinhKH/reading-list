7935d37001b03d5d84240b51d7dfcb916592607d
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-25 22:39:14 +0700

    end project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDlfu8uha4H-7G3HMZUviqQ-JvNIr5-0nw",
@@ -15,5 +16,6 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+const auth = getAuth()
 
-export { db }
+export { db, auth }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,40 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <h2>Login</h2>
+    <form @submit.prevent="handleSubmit">
+        <h2>Login</h2>
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" v-model="email" required>
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email" required>
 
-    <label for="password">Password:</label>
-    <input type="password" name="password" v-model="password" required>
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password" required>
 
-    <button>Login</button>
-  </form>
+        <button>Login</button>
+        <div v-if="error">{{ error }}</div>
+    </form>
 </template>
-
+  
 <script>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import useLogin from '../composables/useLogin'
 
 export default {
-  setup() {
-    const email = ref('')
-    const password = ref('')
+    setup() {
+        const { login, error } = useLogin()
+        const router = useRouter()
 
-    const handleSubmit = async () => {
-      console.log(email.value, password.value)
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = async () => {
+            await login(email.value, password.value)
+
+            if (!error.value) {
+                router.push('/')
+            }
+        }
+
+        return { email, password, handleSubmit, error }
     }
-
-    return { email, password, handleSubmit }
-  }
 }
 </script>
