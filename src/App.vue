a720da23625aecc4b4d12c0f47d830af9aa103e9
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-04 21:35:41 +0700

    A Potential Problem

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,101 @@
 <template>
-  <div class="container">
-    <Navbar />
-    <router-view />
-  </div>
+    <div>
+        <active-element v-if="activeTopic" :topic-title="activeTopic && activeTopic.title"
+            :text="activeTopic && activeTopic.fullText"></active-element>
+        <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-
 export default {
-  components: { Navbar }
-}
+    data() {
+        return {
+            topics: [
+                {
+                    id: 'basics',
+                    title: 'The Basics',
+                    description: 'Core Vue basics you have to know',
+                    fullText:
+                        'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+                },
+                {
+                    id: 'components',
+                    title: 'Components',
+                    description:
+                        'Components are a core concept for building Vue UIs and apps',
+                    fullText:
+                        'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+                },
+            ],
+            activeTopic: null,
+        };
+    },
+    methods: {
+        activateTopic(topicId) {
+            this.activeTopic = this.topics.find((topic) => topic.id === topicId);
+        },
+    },
+};
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: sans-serif;
+}
+
 body {
-  background: #f2f2f2;
+    margin: 0;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+
+section {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 2rem auto;
+    max-width: 40rem;
+    padding: 1rem;
+    border-radius: 12px;
 }
-.container {
-  max-width: 560px;
-  margin: 0 auto;
+
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
 }
-form label, form input {
-  display: block;
+
+li {
+    border-radius: 12px;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    width: 15rem;
+    margin: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
-form input {
-  padding: 4px;
-  margin-top: 10px;
-  margin-bottom: 20px;
+
+h2 {
+    margin: 0.75rem 0;
+    text-align: center;
 }
+
 button {
-  background: #0ec58e;
-  color: #fff;
-  border: 0;
-  border-radius: 6px;
-  padding: 6px;
-  cursor: pointer;
+    font: inherit;
+    border: 1px solid #c70053;
+    background-color: #c70053;
+    color: white;
+    padding: 0.75rem 2rem;
+    border-radius: 30px;
+    cursor: pointer;
+}
+
+button:hover,
+button:active {
+    background-color: #e24d8b;
+    border-color: #e24d8b;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue';
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
+
+const app = createApp(App);
+
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
+
+app.mount('#app');
