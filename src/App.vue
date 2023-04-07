298b31fdf7b7268b6cc28df5398bcc6602d13357
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-07 15:23:14 +0700

    Scoped Slots

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
         <!-- <TheHeader /> -->
         <badge-list></badge-list>
         <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+        <course-goals #default="slotProps">
+            <h2>{{ slotProps.item }}</h2>
+            <p>{{ slotProps['anotherProp'] }}</p>
+        </course-goals>
     </div>
 </template>
 
@@ -11,12 +15,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
     components: {
         TheHeader,
         BadgeList,
-        UserInfo
+        UserInfo,
+        CourseGoals,
     },
     data() {
         return {
