938f96f257fb818d317db5c9a7bcd8b96cf1a36c
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-10 10:42:47 +0700

    Setup & First Component & Props

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,34 @@
 <template>
-  <div class="container">
-    <Navbar />
-    <router-view />
-  </div>
+    <ul>
+        <learning-resource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description"
+            :link="res.link"></learning-resource>
+    </ul>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import LearningResource from './components/learning-resources/LearningResource.vue';
 
 export default {
-  components: { Navbar }
-}
+    components: {
+        LearningResource,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation.',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.org',
+                },
+            ],
+        };
+    },
+};
 </script>
-
-<style>
-body {
-  background: #f2f2f2;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-.container {
-  max-width: 560px;
-  margin: 0 auto;
-}
-form label, form input {
-  display: block;
-}
-form input {
-  padding: 4px;
-  margin-top: 10px;
-  margin-bottom: 20px;
-}
-button {
-  background: #0ec58e;
-  color: #fff;
-  border: 0;
-  border-radius: 6px;
-  padding: 6px;
-  cursor: pointer;
-}
-</style>
