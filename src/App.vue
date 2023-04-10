5e4b4307bb822616b5491ef78281ef33f7cac17f
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-10 15:16:55 +0700

    Header & BaseCard Components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
 <template>
-    <stored-resource :resources="storedResources"></stored-resource>
+	<the-header title="RememberMe"></the-header>
+	<stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResource from './components/learning-resources/StoredResource'
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+import TheHeader from "./components/layouts/TheHeader.vue";
 
 export default {
-    components: {
-        StoredResource,
-    },
-    data() {
-        return {
-            storedResources: [
-                {
-                    id: 'official-guide',
-                    title: 'Official Guide',
-                    description: 'The official Vue.js documentation.',
-                    link: 'https://vuejs.org',
-                },
-                {
-                    id: 'google',
-                    title: 'Google',
-                    description: 'Learn to google...',
-                    link: 'https://google.org',
-                },
-            ],
-        };
-    },
+	components: {
+		StoredResources,
+		TheHeader,
+	},
+	data() {
+		return {
+			storedResources: [
+				{
+					id: "official-guide",
+					title: "Official Guide",
+					description: "The official Vue.js documentation.",
+					link: "https://vuejs.org",
+				},
+				{
+					id: "google",
+					title: "Google",
+					description: "Learn to google...",
+					link: "https://google.org",
+				},
+			]
+		};
+	},
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
 * {
-    box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 html {
-    font-family: 'Roboto', sans-serif;
+	font-family: "Roboto", sans-serif;
 }
 
 body {
-    margin: 0;
+	margin: 0;
 }
 </style>
--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,51 +1,52 @@
 <template>
-    <li>
-        <div>
-            <header>
-                <h3>{{ title }}</h3>
-                <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ description }}</p>
-        <nav>
-            <a :href="link">View Resource</a>
-        </nav>
-    </li>
+	<li>
+		<base-card>
+			<header>
+				<h3>{{ title }}</h3>
+				<button>Delete</button>
+			</header>
+			<p>{{ description }}</p>
+			<nav>
+				<a :href="link">View Resource</a>
+			</nav>
+		</base-card>
+	</li>
 </template>
 
 <script>
 export default {
-    props: ['title', 'description', 'link']
-}
+	props: ["title", "description", "link"],
+};
 </script>
+
 <style scoped>
 li {
-    margin: auto;
-    max-width: 40rem;
+	margin: auto;
+	max-width: 40rem;
 }
 
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 
 h3 {
-    font-size: 1.25rem;
-    margin: 0.5rem 0;
+	font-size: 1.25rem;
+	margin: 0.5rem 0;
 }
 
 p {
-    margin: 0.5rem 0;
+	margin: 0.5rem 0;
 }
 
 a {
-    text-decoration: none;
-    color: #ce5c00;
+	text-decoration: none;
+	color: #ce5c00;
 }
 
 a:hover,
 a:active {
-    color: #c89300;
+	color: #c89300;
 }
 </style>
