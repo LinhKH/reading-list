3c181905b4a825d5b339249b3fb122228c5550c6
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-18 23:12:27 +0700

    Namespacing Modules

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createStore } from 'vuex';
 import App from './App.vue';
 
 const counterModule = {
+    namespaced: true,
     state() {
         return {
             counter: 0
@@ -26,9 +27,13 @@ const counterModule = {
         increase(context, payload) {
             console.log(context);
             context.commit('increase', payload);
-        }
+        },
+        login() { }
     },
     getters: {
+        testAuth(state) {
+            return state.isLoggedIn;
+        },
         finalCounter(state) {
             return state.counter * 3;
         },
