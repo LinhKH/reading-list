1cc44a181f66a6b2cc124275b2782def7f51b3a9
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-04-18 16:43:10 +0700

    Example: Adding More State

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import App from './App.vue';
 const store = createStore({
     state() {
         return {
-            counter: 0
+            counter: 0,
+            isLoggedIn: false
         };
     },
     mutations: {
@@ -15,11 +16,13 @@ const store = createStore({
         },
         increase(state, payload) {
             state.counter = state.counter + payload.value;
+        },
+        setAuth(state, payload) {
+            state.isLoggedIn = payload.isAuth;
         }
     },
     actions: {
         increment(context) {
-            console.log(context)
             setTimeout(function () {
                 context.commit('increment');
             }, 2000);
@@ -27,6 +30,12 @@ const store = createStore({
         increase(context, payload) {
             console.log(context);
             context.commit('increase', payload);
+        },
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
         }
     },
     getters: {
@@ -42,6 +51,9 @@ const store = createStore({
                 return 100;
             }
             return finalCounter;
+        },
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
         }
     }
 });
